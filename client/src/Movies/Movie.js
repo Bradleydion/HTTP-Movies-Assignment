c4a8f147b2e58e729bff84484d9c756ac7bd8fbb
Bradley Dion <bradleydion@MacBook-Pro.local>
2021-01-19 18:54:11 -0500

    with heavy help, I was able to complete this assignment t o turn in so that I could attend to personal things durring class time this evening

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import MovieCard from "./MovieCard";
+import {Button, ButtonGroup} from 'reactstrap';
 
-function Movie({ addToSavedList }) {
+function Movie({ addToSavedList, deleteMovie }) {
   const [movie, setMovie] = useState(null);
   const params = useParams();
+  const history = useHistory();
 
   const fetchMovie = (id) => {
     axios
@@ -17,6 +19,16 @@ function Movie({ addToSavedList }) {
   const saveMovie = () => {
     addToSavedList(movie);
   };
+  
+  const updateMovie = () => {
+    history.push(`/update-movie/${params.id}`)
+  }
+  
+  const handleDelete = () => {
+    console.log('deleting movie', params.id)
+    deleteMovie(params.id);
+    history.push('/');
+  }
 
   useEffect(() => {
     fetchMovie(params.id);
@@ -33,6 +45,10 @@ function Movie({ addToSavedList }) {
       <div className="save-button" onClick={saveMovie}>
         Save
       </div>
+      <ButtonGroup size='md'>
+        <Button  color='primary' onClick={updateMovie}>Update Movie</Button>
+        <Button  color='secondary' onClick={handleDelete}>Delete Movie</Button>
+      </ButtonGroup>
     </div>
   );
 }
